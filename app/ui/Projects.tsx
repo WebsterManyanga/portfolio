f2f0c36260e1b7a932d494cb4fbaae05438699d7
Webster <strategywebstermanyanga@gmail.com>
2024-04-18 14:06:43 +0200

    Add carousel to projects page
    Used Embla Carousel to add a carousel to the projects page.

--- app/ui/Projects.tsx
+++ app/ui/Projects.tsx
@@ -1,11 +1,30 @@
+"use client";
+
+import useEmblaCarousel from "embla-carousel-react";
+import "./Projects.css";
+
 const Projects = () => {
   // Carousel of projects
+  const [emblaRef] = useEmblaCarousel();
+
+  const project = (
+    <div className="embla__slide  h-full flex justify-between px-4  pt-20 relative">
+    <div className="block w-full h-3/4 bg-gray-400"></div>
+    <div className="absolute bottom-6 right-10 text-6xl font-extrabold">
+      <h2 className="text-end">01</h2>
+      <h2>PROJECT</h2>
+    </div>
+  </div>
+  );
+
   return (
-    <section className="h-full flex justify-between mt-5 px-4  pt-20 relative">
-      <div className="block w-full h-3/4 bg-gray-400"></div>
-      <div className="absolute bottom-6 right-10 text-6xl font-extrabold">
-        <h2 className="text-end">01</h2>
-        <h2>PROJECT</h2>
+    <section className="embla  h-full" ref={emblaRef}>
+      <div className="embla__container h-full">
+        {project}
+        {project}
+        {project}
+        {project}
+        {project}
       </div>
     </section>
   );

70250d815490338386d5334615bda3ddd77487e1
Webster <strategywebstermanyanga@gmail.com>
2024-04-21 08:40:08 +0200

    create nav bar for larger devices

---
+++ app/ui/Nav.tsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+  return (
+    <nav className='hidden lg:flex sticky top-0 font-semibold px-10 gap-72 justify-center pt-6 bg-black z-10 bg-opacity-60'>
+    <a href="" >PROJECTS</a>
+    <a href="">WORK EXPERIENCE</a>
+    <a href="">CERTIFICATIONS</a>
+    <a href="">CONTACT ME</a>
+
+  </nav>
+
+  );
+}
+
+export default Nav;

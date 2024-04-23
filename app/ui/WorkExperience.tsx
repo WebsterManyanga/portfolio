15dd4a2fa9b6190ee8cf7e40585c4ac0e70d97c6
Webster <strategywebstermanyanga@gmail.com>
2024-04-23 10:50:19 +0200

    make work experience page responsive on large screens

--- app/ui/WorkExperience.tsx
+++ app/ui/WorkExperience.tsx
@@ -1,6 +1,6 @@
 const WorkExperience = () => {
   const job = (
-    <div className=" before:content-[''] before:block before:w-1 before:h-40 before:bg-white relative before:absolute before:-left-6 before:top-10">
+    <div className="lg:mt-5 before:content-[''] before:block before:w-1 before:h-40 lg:before:h-24 before:bg-white relative before:absolute before:-left-6 before:top-10">
       <h2 className="relative font-extrabold before:content-[''] before:block before:w-2 before:h-2 before:bg-white before:rounded-full before:absolute before:-left-6 before:top-2">Customer Service Representative</h2>
       <p className="font-light text-sm">Feb 2022 - Feb 2023</p>
       <p className="font-light text-sm">Foundever</p>
@@ -17,8 +17,11 @@ const WorkExperience = () => {
   );
 
   return (
-    <section className="h-full overflow-scroll  mt-5 px-10 pt-20">
+    <section className="h-full overflow-scroll  mt-5 px-10 pt-20 lg:flex  flex-col justify-center lg:pl-96 lg:pb-10 ">
       {job} {job} {job}{" "}
+      <h1 className="text-5xl font-extrabold lg:text-6xl  justify-center items-center h-full lg:flex hidden">
+        WORK EXPERIENCE.
+      </h1>
     </section>
   );
 };

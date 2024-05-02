ac99efee3f24b7710ec3e18788425f3a83882b42
Webster <strategywebstermanyanga@gmail.com>
2024-05-02 15:47:22 +0200

    added click me btn to test video play

--- app/ui/Certifications.tsx
+++ app/ui/Certifications.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
 
 const Certifications = () => {
   const [emblaRef] = useEmblaCarousel();
   const certifcation = (
     <div className="embla__slide  h-full  lg:flex flex-col items-center px-4  pt-20 ">
-      <div className="block w-full lg:w-2/5 lg:h-2/3 h-1/2 bg-gray-400"></div>
+      <div className="block w-full lg:w-2/5 lg:h-2/3 h-1/2">
+        <Image alt="certification" src="/javascriptAlgorithms.jpg" width={500} height={600} className="w-full h-auto" />
+      </div>
       <div className="text-6xl font-extrabold lg:hidden">
         <h2 className="text-end">01</h2>
         <h2 >Algorithms & Data Structures</h2>
--- app/ui/Projects.tsx
+++ app/ui/Projects.tsx
@@ -1,23 +1,29 @@
 "use client";
 import useEmblaCarousel from "embla-carousel-react";
+import { useRef } from "react";
 
 const Projects = () => {
   // Carousel of projects
   const [emblaRef] = useEmblaCarousel();
+  const myRef = useRef<HTMLVideoElement>(null);
   
+  function handleClick() {
+    console.log('clicked');
+    myRef.current?.play();
+  }
 
   const project = (
     <div className="embla__slide  h-full flex lg:gap-10 px-4   pt-20 relative " id="projects">
-      <div className="block w-full lg:w-1/4 h-3/4">
-        <video width="320" height="240"  muted autoPlay preload="auto"  >
+      <div className="block w-fit lg:w-1/4 h-fit border-white border-4 ">
+        <video width="320" height="240"  muted  preload="auto" controls ref={myRef} >
           <source src="/blackeye.MP4" type="video/mp4"/>
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="absolute bottom-6 right-10 text-6xl font-extrabold lg:left-60 lg:bottom-16 lg:right-auto">
+      {/* <div className="absolute bottom-6 right-10 text-6xl font-extrabold lg:left-60 lg:bottom-16 lg:right-auto">
         <h2 className="text-end">01</h2>
         <h2>PROJECT</h2>
-      </div>
+      </div> */}
       <p className="hidden lg:block w-1/2">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
         maiores nihil iusto ipsum ad quasi repellendus odio cumque dignissimos
@@ -37,6 +43,7 @@ const Projects = () => {
   return (
     <section className="embla snap-start lg:w-[80vw] lg:mx-auto h-full" ref={emblaRef}>
       <div className="embla__container h-full">
+        <button className="block w-full bg-red-400" onClick={handleClick}>Click Me!</button>
         {project}
         {project}
         {project}

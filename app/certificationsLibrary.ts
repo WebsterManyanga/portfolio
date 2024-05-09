2b37286996971ef200cd1d7c0651249870427c17
Webster <strategywebstermanyanga@gmail.com>
2024-05-09 21:50:40 +0200

    added certifications and updated styling

---
+++ app/certificationsLibrary.ts
@@ -0,0 +1,23 @@
+export const certifications:certificationType[] = [
+  {
+    title: "Legacy Algorithms & Data Structures",
+    src: "/certifications/javascriptAlgorithms.jpg"
+  },
+  {
+    title: "Programming with JavaScript",
+    src: "/certifications/programmingWithJS.jpg"
+  },
+  {
+    title: "React Basics",
+    src: "/certifications/reactBasics.jpg"
+  },
+  {
+    title: "Responsive Web Design",
+    src: "/certifications/responsiveWebDesign.jpg"
+  }
+]
+
+interface certificationType {
+  title: string;
+  src: string;
+}
--- app/ui/Certifications.tsx
+++ app/ui/Certifications.tsx
@@ -2,28 +2,29 @@
 
 import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
+import { certifications } from "../certificationsLibrary";
 
 const Certifications = () => {
   const [emblaRef] = useEmblaCarousel();
-  const certifcation = (
-    <div className="embla__slide  h-full  lg:flex flex-col items-center px-4  pt-20 ">
-      <div className="block w-full lg:w-2/5 lg:h-2/3 h-1/2">
-        <Image alt="certification" src="/javascriptAlgorithms.jpg" width={500} height={600} className="w-full h-auto" />
+  const certificationsLibrary = certifications.map((certification, i) => {
+    return (
+      <div className="embla__slide  h-full  lg:flex flex-col items-center px-4  pt-20 ">
+        <div className="block w-full lg:w-2/5 lg:h-2/3 h-1/2">
+          <Image alt={certification.title} src={certification.src} width={500} height={600} className="w-full h-auto" />
+        </div>
+        <div className="text-3xl font-extrabold lg:hidden">
+          <h2 className="text-start">{`0${i+1}`}</h2>
+          <h2 >{certification.title}</h2>
+        </div>
       </div>
-      <div className="text-6xl font-extrabold lg:hidden">
-        <h2 className="text-end">01</h2>
-        <h2 >Algorithms & Data Structures</h2>
-      </div>
-    </div>
-  );
+    );
+  });
+
 
   return (
     <section className="embla snap-start relative h-full" id="certifications" ref={emblaRef}>
       <div className="embla__container h-full ">
-        {certifcation}
-        {certifcation}
-        {certifcation}
-        {certifcation}
+        {certificationsLibrary}
       </div>
       <h1 className="text-5xl lg:absolute   lg:w-full bottom-36 lg:h-fit font-extrabold lg:text-6xl  justify-center items-center h-full lg:flex hidden">
         CERTIFICATIONS

b10d881c8d9efaf4f9edefbba8e64ca3cb2439a7
Webster <strategywebstermanyanga@gmail.com>
2024-05-11 08:21:04 +0200

    contents page responsive

--- app/ui/Contents.tsx
+++ app/ui/Contents.tsx
@@ -1,24 +1,24 @@
 const Contents = () => {
   return (
-    <section className="h-full snap-start  flex justify-between mt-5 px-4  lg:w-3/4 lg:justify-start lg:gap-96 lg:px-56 mx-auto">
+    <section className="h-full snap-start  flex justify-between mt-5 px-4  lg:w-3/4  lg:justify-start lg:gap-96  mx-auto">
       <div className="flex justify-center items-center w-16">
         <h1 className="text-6xl -rotate-90 font-extrabold block lg:text-8xl ">CONTENTS.</h1>
       </div>
-      <div className="flex flex-col gap-20 lg:gap-56 justify-center items-center lg:flex-row ">
+      <div className="flex flex-col gap-20  justify-center items-center lg:flex-row ">
         <a href='#projects' className="flex flex-col justify-center items-center">
-          <h2 className=" font-extrabold text:6xl lg:text-8xl">01</h2>
+          <h2 className=" font-extrabold lg:text-6xl">01</h2>
           <p className="font-light text-sm">PROJECTS</p>
         </a>
-        <a href='#workExperience' className="flex flex-col justify-center items-center">
-          <h2 className=" font-extrabold text:6xl lg:text-8xl">02</h2>
-          <p className="font-light text-sm">WORK EXPERIENCE</p>
+        <a href='#workExperience' className="flex flex-col justify-center items-center ">
+          <h2 className=" font-extrabold lg:text-6xl">02</h2>
+          <p className="font-light text-sm">EXPERIENCE</p>
         </a>
         <a href='#certifications' className="flex flex-col justify-center items-center">
-          <h2 className=" font-extrabold text:6xl lg:text-8xl">03</h2>
+          <h2 className=" font-extrabold lg:text-6xl">03</h2>
           <p className="font-light text-sm">CERTIFICATIONS</p>
         </a>
         <a href='#footer' className="flex flex-col justify-center items-center">
-          <h2 className=" font-extrabold text:6xl lg:text-8xl">04</h2>
+          <h2 className=" font-extrabold lg:text-6xl">04</h2>
           <p className="font-light text-sm">CONTACT</p>
         </a>
       </div>
--- app/ui/Projects.tsx
+++ app/ui/Projects.tsx
@@ -10,36 +10,32 @@ const Projects = () => {
   const vidRef = useRef<HTMLVideoElement>(null);
 
   const projects = projectsLibrary.map((project, i) => (
-    <div
-      className="embla__slide  h-full flex lg:gap-10 px-4   pt-20 relative "
-      id="projects"
-    >
-      <div className="block w-fit  h-fit border-white border-4 ">
-        <VideoTest project={project} />
+    <div className="embla__slide h-full w-screen">
+      <div className="flex lg:gap-10 px-4   pt-20 " id="projects">
+        <div className="block w-fit  h-fit border-white border-4 ">
+          <VideoTest project={project} />
+        </div>
+
+        <p className="hidden lg:block w-[450px]">
+          {project.description}
+          <span className="block mt-20">
+            Utilized: {project.technologies.join(", ")}
+          </span>
+          <div className="hidden lg:block  bottom-6 right-10 font-extrabold lg:left-60 lg:bottom-10 lg:right-auto">
+            <h2>{`0${i + 1}`}</h2>
+            <h2>{project.name}</h2>
+          </div>
+        </p>
       </div>
-
-      <div className="hidden lg:block absolute bottom-6 right-10 text-6xl font-extrabold lg:left-60 lg:bottom-16 lg:right-auto">
-      <h2 className="text-end">{`0${i+1}`}</h2>
-      <h2>{project.name}</h2>
-    </div> 
-      <p className="hidden lg:block w-[450px]">
-        {project.description}
-        <span className="block mt-20">Utilized: {project.technologies.join(", ")}</span>
-
-      </p>
-
     </div>
   ));
-
 
   return (
     <section
       className="embla snap-start lg:w-[80vw] lg:mx-auto h-full"
       ref={emblaRef}
     >
-      <div className="embla__container h-full">
-        {projects}
-      </div>
+      <div className="embla__container h-full">{projects}</div>
     </section>
   );
 };

532ecb11587c667c612032078bfb2736da74c669
Webster Manyanga <109490186+WebsterManyanga@users.noreply.github.com>
2024-06-14 07:43:34 +0000

    adjusted button margin for iphones

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <p className="text-sm">
       WEB DEVELOPER
     </p>
-    <a href="./WebsterManyanga_Resume.pdf" className='absolute bottom-30 sm:inline-block mt-20 sm:mt-10 rounded bg-neutral-100 px-2 sm:px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-600 shadow-light-3 transition duration-150 ease-in-out hover:bg-neutral-50 hover:shadow-light-2 hover:text-neutral-800 focus:bg-neutral-200 focus:shadow-light-2 focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-light-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong '>Download Resume</a>
+    <a href="./WebsterManyanga_Resume.pdf" className='absolute bottom-24 sm:inline-block mt-20 sm:mt-10 rounded bg-neutral-100 px-2 sm:px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-600 shadow-light-3 transition duration-150 ease-in-out hover:bg-neutral-50 hover:shadow-light-2 hover:text-neutral-800 focus:bg-neutral-200 focus:shadow-light-2 focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-light-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong '>Download Resume</a>
     <div className=" hidden lg:block absolute bottom-20 right-20">  
       <Image
         src={autograph}
